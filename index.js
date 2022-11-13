3458680ff39d9de27987418b1bbf042200aa9bda
Driven Education <flaviaestacheski@gmail.com>
2022-11-13 18:23:55 -0300

    organized project structure

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 // Top-Level Await
+
 try {
     await mongoClient.connect()
 } catch(err) {
@@ -16,8 +17,33 @@ try {
 }
 let db = mongoClient.db("batepapouol");
 
+//  Routes
 
+app.get('/participants', async (req, res) => {
 
+});
+
+app.post('/participants', async (req, res) => {
+
+});
+
+//
+
+app.get('/messages', async (req, res) => {
+
+});
+
+app.post('/messages', async (req, res) => {
+
+});
+
+//
+
+app.post('/status', async (req, res) => {
+
+})
+
+//
 
 app.listen(process.env.PORT, () =>
     console.log(`App running in port: ${process.env.PORT}`)
